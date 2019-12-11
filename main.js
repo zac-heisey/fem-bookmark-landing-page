3f9389ca949f601ebc4e4cff34a546c5be19ba21
Zac Heisey <zac.heisey@alextom.com>
2019-12-11 12:45:32 -0500

    replace svg menu icons with css

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //== Variables ==//
 
-var hamburgerIcon = document.querySelector('#hamburger-icon');
-var closeIcon = document.querySelector('#close-icon');
+var menuIcon = document.querySelector('#menu-icon');
 var navOverlay = document.querySelector('#nav-overlay');
 var logoText = document.querySelector('#logo-text');
 
@@ -9,21 +8,22 @@ var logoText = document.querySelector('#logo-text');
 
 // Mobile menu toggle
 function menuToggle(event) {
-  // Bail if click is NOT on hamburger or close icons
-  if (event.target !== hamburgerIcon && event.target !== closeIcon) return;
-  // If click is on hamburger icon, show menu items & change logo text to white
-  if (event.target == hamburgerIcon) {
+
+  // If menu is not open, show menu items & change logo text to white
+  if (!menuIcon.classList.contains('open')) {
+    menuIcon.classList.add('open');
     navOverlay.style.height = '100%';
     logoText.style.fill = '#fff';
-  }
-  // If click is on close icon, hide menu items & return logo text to default color
-  if (event.target == closeIcon) {
+  // Else close menu and return logo text to default  
+  } else {
+    menuIcon.classList.remove('open');
     navOverlay.style.height = '0%';
     logoText.style.fill = 'initial';
   }
+
 }
 
 //== Event Listeners ==//
 
-// Listen for click events on document
-document.addEventListener('click', menuToggle, false);
+// Listen for click events on menu icon
+menuIcon.addEventListener('click', menuToggle, false);
