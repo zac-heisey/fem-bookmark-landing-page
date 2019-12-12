21f8cf0e6a0e923243e3c6fb77d2cc1ceb021ea7
Zac Heisey <zac.heisey@alextom.com>
2019-12-12 17:18:34 -0500

    work on feature tabs toggle functionality

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 //== Variables ==//
 
+// Menu Icon
 var menuIcon = document.querySelector('#menu-icon');
+// Nav Overlay
 var navOverlay = document.querySelector('#nav-overlay');
+// SVG Logo Text
 var logoText = document.querySelector('#logo-text');
+// Feature Tabs (NodeList)
+var featureTabs = document.querySelectorAll('[data-tab]');
 
 //== Functions ==//
 
@@ -14,7 +19,7 @@ function menuToggle(event) {
     menuIcon.classList.add('open');
     navOverlay.style.height = '100%';
     logoText.style.fill = '#fff';
-  // Else close menu and return logo text to default  
+  // Else close menu and return logo text to default
   } else {
     menuIcon.classList.remove('open');
     navOverlay.style.height = '0%';
@@ -23,7 +28,35 @@ function menuToggle(event) {
 
 }
 
+// Feature tabs toggle
+function featureTabsToggle(event) {
+
+  // Get current value of hash (minus the #)
+  var hash = window.location.hash.substring(1);
+
+  // If hash is blank, make first feature tab active (initial state)
+  if (hash === '') {
+    featureTabs[0].classList.add('active');
+    return;
+  }
+
+  // Loop through feature tabs
+  for (var tab of featureTabs) {
+    // Remove active class from any feature tabs
+    if (tab.classList.contains('active')) {
+      tab.classList.remove('active');
+    }
+    // If [data-tab] attribute value matches hash value, add active class
+    if (tab.getAttribute('data-tab') === hash) {
+      tab.classList.add('active');
+    }
+  }
+
+}
+
 //== Event Listeners ==//
 
 // Listen for click events on menu icon
 menuIcon.addEventListener('click', menuToggle, false);
+// Listen for hashchange event on window
+window.addEventListener('hashchange', featureTabsToggle, false);
